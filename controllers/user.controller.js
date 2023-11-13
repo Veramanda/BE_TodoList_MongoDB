933f97f1806bdc3faebd706a523820c1d7b2d7d6
Veramanda <faidatun237@gmail.com>
2023-11-13 13:00:32 +0700

    menambahkan user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,12 @@ const User = require('../models/user');
 
 module.exports = {
   getAllUser: async (req, res) => {
-    
+    const users = await User.find()
+
+    res.json({
+      message: "berhasil mendapatkan data user",
+      data: users
+    })
   },
 
   getUserById:(req, res) => {
@@ -10,6 +15,12 @@ module.exports = {
   },
 
   createUser:  async (req, res) => {
-    
+    let data = req.body
+
+    await User.create(data)
+
+    res.json({
+      message: "berhasil membuat data user"
+    })
   },
 }
